8956f562fc59b4bae312096ff007f046fc0d4974
souleymane1234 <konatesouleymane42@gmail.com>
2023-02-22 10:52:14 +0100

    view stories terminer mais pas fonctionnel pour l'instant

---
+++ src/donnee/storiesData.js
@@ -0,0 +1,33 @@
+export const storiesData = [
+    {
+        id: 2,
+        image: require('../assets/imgConcert1.jpeg'),
+        nomEvenement: 'Concert Ed Sheeran',
+    },
+    {
+        id: 3,
+        image: require('../assets/imgConcert1.jpeg'),
+        nomEvenement: 'Concert Ed Sheeran',
+    },
+    {
+        id: 4,
+        image: require('../assets/imgConcert1.jpeg'),
+        nomEvenement: 'Concert Ed Sheeran',
+    },
+    {
+        id: 5,
+        image: require('../assets/imgConcert1.jpeg'),
+        nomEvenement: 'Concert Ed Sheeran',
+    },
+    {
+        id: 6,
+        image: require('../assets/imgConcert1.jpeg'),
+        nomEvenement: 'Concert Ed Sheeran',
+    },
+    {
+        id: 7,
+        image: require('../assets/imgConcert1.jpeg'),
+        nomEvenement: 'Concert Ed Sheeran',
+    },
+    
+];
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -6,9 +6,13 @@ import {
   StyleSheet,
   Text,
   View,
-  Dimensions
+  Dimensions,
+  Image,
+  FlatList
 } from 'react-native';
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { StoriesComponent } from '../../components/storiesComponent';
+import { storiesData } from '../../donnee/storiesData';
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
@@ -16,27 +20,63 @@ const windowHeight = Dimensions.get("window").height;
 
 const HomeScreen = () => {
 
+
+  const renderItems = ({ item }) => {
+    return (
+      <StoriesComponent
+          nomEvenement={item.nomEvenement}
+          image={item.image}
+        />
+
+    )
+  }
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar animated={true} backgroundColor="#000" />
+      {/* header start  */}
       <View style={styles.header}>
-        <View style={styles.headerView}>
-                  <Icon
-        style={{ alignSelf: "center" }}
-        name="magnify"
-        pack="material"
-        size={20}
-        color={"#DA7200"}
-      />
+        <View style={[styles.headerView, {flexDirection: "row", justifyContent: "space-between"}]}>
+          <Icon
+            style={{ alignSelf: "center" }}
+            name="heart-outline"
+            pack="material"
+            size={20}
+            color={"#fff"}
+          />
+          <Icon
+            style={{ alignSelf: "center", marginHorizontal: 5 }}
+            name="bookmark-multiple-outline"
+            pack="material"
+            size={20}
+            color={"#fff"}
+          />
         </View>
         <View style={styles.headerView}>
             <Text style={[styles.headerViewText, {fontWeight: "bold"}]}>Scan Me</Text>
         </View>
         <View style={styles.headerView}>
-            <Text style={styles.headerViewText}>Icon</Text>
+          <Icon
+            style={{ alignSelf: "center" }}
+            name="wallet"
+            pack="material"
+            size={20}
+            color={"#fff"}
+          />
         </View>
       </View>
-      <Text>welcome to you</Text>
+      {/* header end  */}
+      {/* stories View start  */}
+      <View style={styles.storiesView}>
+        <Text style={{color: "#717c82"}}>EN VEDETTE</Text>
+        <FlatList
+          style={{}}
+          horizontal={true}
+          data={storiesData}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={renderItems}
+        />
+      </View>
+      {/* stories View end  */}
     </SafeAreaView>
   );
 };
@@ -62,6 +102,9 @@ const styles = StyleSheet.create({
     headerViewText:{
         color: "#fff",
         fontSize: 18
+    },
+    storiesView: {
+      margin: 10,
     }
 
 });
